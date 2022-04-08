b9fbdedc25e464cef83a6c766d7674c4730d25b6
Germiprogramer <llorentemunozgerman@gmail.com>
2022-04-08 13:14:56 +0200

    apartado 1 completo

--- apartado1.py
+++ apartado1.py
@@ -1,5 +1,23 @@
 import pandas as pd
 
-datos = pd.read_csv("calificaciones.csv", header=0)
+class Calificaciones():
+    def __init__(self, archivo):
+        self.archivo = archivo
 
-print(datos)
+    def creardiccionario(self):
+        datos = pd.read_csv(self.archivo, header=0)
+        
+        parcial_1 = list(datos["Parcial1"])
+        parcial_2 = list(datos["Parcial2"])
+        asistencia = list(datos["Asistencia"])
+        nombre = list(datos["Nombre"])
+
+        for i in range(1,17):
+            diccionario = {"Nota primer parcial {}".format(nombre[i]) : parcial_1[i] , "Nota segundo parcial {}".format(nombre[i]) : parcial_2[i] , "Asistencia" : asistencia[i]}
+
+
+
+
+
+csv = Calificaciones("calificaciones.csv")
+csv.creardiccionario()
